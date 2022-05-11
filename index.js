c45f4d3777e9622285ee3132a483c384239ed236
Cerina Malaluan <cerinamalaluan@Cerinas-MBP.hsd1.ca.comcast.net>
2022-05-11 12:38:06 -0700

    commit for the game

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-window.prompt('Do you head left or right?')
+let firstAnswer  = window.prompt('Do you head left or right?')
+if(firstAnswer === 'left'){
+    let secondAnswer = window.prompt(`You come across a stray cat. 
+It scampers off down a small hole, just large enough for you to crawl through. 
+Do you follow it, or continue on your path?`)
+
+} else if(firstAnswer === 'right') {
+    let secondAnswer = window.prompt(`You come across a snoring dragon. 
+On the other side of him, you see a shiny chest of treasure. Another path would 
+lead you away from the dragon altogether. Which path do you take?`)
+
+if(secondAnswer === `follow`){window.prompt("YOu have a new cat")}
+else if(secondAnswer ===`continue`)
+{`You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?}
